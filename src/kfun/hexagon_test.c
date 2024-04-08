734752855a50edba80ff78255d5ae169e7c81d01
neverbot <ivan@neverbot.com>
2024-04-08 19:23:31 +0200

    chore: test new kfun

---
+++ src/kfun/hexagon_test.c
@@ -0,0 +1,37 @@
+/*
+ * hexagon_test() kfun extension
+ */
+
+# include "lpc_ext.h"
+
+static void hexagon_test(LPC_frame f, int nargs, LPC_value retval)
+{
+  // LPC_value val;
+  LPC_string str;
+  LPC_dataspace data;
+  char *p;
+  unsigned int i;
+
+  /* fetch the argument string */
+  // val = lpc_frame_arg(f, nargs, 0);
+  // str = lpc_string_getval(val);
+
+  data = lpc_frame_dataspace(f);
+  str = lpc_string_new(data, "Hexagon Mudlib extension test", 29);
+
+  /* put result in return value */
+  lpc_string_putval(retval, str);
+}
+
+static char hexagon_test_proto[] = { LPC_TYPE_STRING, LPC_TYPE_VOID, 0 };
+static LPC_ext_kfun kf[1] = {
+  "hexagon_test",
+  hexagon_test_proto,
+  &hexagon_test
+};
+
+int lpc_ext_init(int major, int minor, char *config)
+{
+  lpc_ext_kfun(kf, 1);
+  return 1;
+}
